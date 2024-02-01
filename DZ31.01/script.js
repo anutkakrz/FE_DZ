964fac9056d5637b072122d8a3f83639eafda12e
Hanna Krzh <anutkakrz@gmail.com>
2024-02-02 00:47:17 +0200

    1.02

---
+++ DZ31.01/script.js
@@ -0,0 +1,33 @@
+//Задача 1
+/*const num = +prompt("Введите число")
+console.log (num/100*10)*/
+
+//Задача 2
+/*const num1 = +prompt("Введите 1 число")
+const num2 = +prompt("Введите 2 число")
+if(num1<num2) {
+   console.log (num1)
+}
+else {console.log (num2)}*/
+
+//Задача 3
+/*const num = +prompt("Введите число")
+if(num>100) {
+   console.log("Число больше 100")
+}
+else if(num===100) {
+   console.log("Число равно 100")
+}
+else {
+   console.log("Число меньше 100")
+}100*/
+
+//Задача 4
+/*const name = prompt("Введите ваше имя")
+const age = +prompt("Сколько вам лет?")
+if(age>17) {
+   console.log(`Hello, ${name}`)
+}
+else {
+   console.log(`Hi, ${name}`)
+}*/
